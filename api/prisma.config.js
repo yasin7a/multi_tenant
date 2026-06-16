929d023f7dc745f43911ec22a8925088d5fa66c5
yasin7a <arafath7yasin@gmail.com>
2026-06-16 16:08:20 +0600

    dd

--- api/prisma.config.js
+++ api/prisma.config.js
@@ -2,9 +2,9 @@ import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-  schema: 'api/prisma/schema.prisma',
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: 'api/prisma/migrations',
+    path: 'prisma/migrations',
   },
   datasource: {
     url: env('DATABASE_URL'),
